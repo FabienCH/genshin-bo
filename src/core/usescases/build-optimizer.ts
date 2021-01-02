8d7c257a7aa8b400ea22ae3cd157827d2b1232f3
FabienCH <fabien.charvaz@gmail.com>
2021-01-02 21:11:14 +0100

    refactor(build-optimizer): add update build stat method

--- src/core/usescases/build-optimizer.ts
+++ src/core/usescases/build-optimizer.ts
@@ -7,16 +7,16 @@ export class BuildOptimizer {
   public computeBuildStats(artifacts: Artifact[]): BuildStatisticsValues {
     return artifacts.reduce((buildStats, artifact: Artifact) => {
       const mainStatKey: PossibleMainStats = Object.keys(artifact.mainStat)[0] as PossibleMainStats;
-      buildStats[mainStatKey] = buildStats[mainStatKey]
-        ? buildStats[mainStatKey] + artifact.mainStat[mainStatKey]
-        : artifact.mainStat[mainStatKey];
+      buildStats[mainStatKey] = this.getUpdatedBuildStat(buildStats[mainStatKey], artifact.mainStat[mainStatKey]);
       Object.keys(artifact.subStats).forEach((subStatKey: PossibleSubStats) => {
-        buildStats[subStatKey] = buildStats[subStatKey]
-          ? buildStats[subStatKey] + artifact.subStats[subStatKey]
-          : artifact.subStats[subStatKey];
+        buildStats[subStatKey] = this.getUpdatedBuildStat(buildStats[subStatKey], artifact.subStats[subStatKey]);
       });
 
       return buildStats;
     }, {} as BuildStatisticsValues);
+  }
+
+  private getUpdatedBuildStat(buildStat: number, artifactStat: number): number {
+    return buildStat ? buildStat + artifactStat : artifactStat;
   }
 }
