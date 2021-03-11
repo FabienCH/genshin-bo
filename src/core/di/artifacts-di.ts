2a73d0fcf11168b60e207ba58721feea19668673
FabienCH <fabien.charvaz@gmail.com>
2021-03-11 13:25:12 +0100

    refactor(artifacts): improve artifact repository DI

--- src/core/di/artifacts-di.ts
+++ src/core/di/artifacts-di.ts
@@ -1,6 +1,21 @@
 import { InMemoryArtifactsRepository } from '../adapters/secondaries/in-memory-artifacts-repository';
+import { ArtifactsRepository } from '../domain/artifacts-repository';
+import { AllArtifactsData } from '../domain/models/artifact-data';
 import { ArtifactsHandler } from '../usescases/artifacts-handler';
 
-export const ArtifactsDI = {
-  artifactsHandler: new ArtifactsHandler(new InMemoryArtifactsRepository()),
-};
+export abstract class ArtifactsDI {
+  public static artifactsHandler: ArtifactsHandler = new ArtifactsHandler();
+
+  private static artifactsRepository: ArtifactsRepository;
+
+  public static registerRepository(artifactsData?: AllArtifactsData): void {
+    ArtifactsDI.artifactsRepository = new InMemoryArtifactsRepository(artifactsData);
+  }
+
+  public static getRepository(): ArtifactsRepository {
+    if (ArtifactsDI.artifactsRepository) {
+      return ArtifactsDI.artifactsRepository;
+    }
+    throw new Error('Artifacts repository is not registered.');
+  }
+}
