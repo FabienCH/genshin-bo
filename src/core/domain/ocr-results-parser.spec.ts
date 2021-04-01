00055e0210637c5db879f0d8becb19e2e2fddfc4
FabienCH <fabien.charvaz@gmail.com>
2021-04-01 15:21:19 +0200

    feat(artifacts-importer): parse ocr artifact subs stats

--- src/core/domain/ocr-results-parser.spec.ts
+++ src/core/domain/ocr-results-parser.spec.ts
@@ -1,5 +1,6 @@
 import { importedArtifactDataMock } from '../../test/imported-artifacts-data-mock';
 import { goodOcrResultsMock } from '../../test/ocr-results-mock';
+import { SubStats } from './models/sub-statistics';
 import { OcrResultsParser } from './ocr-results-parser';
 
 fdescribe('OcrResultsParser', () => {
@@ -18,6 +19,22 @@ fdescribe('OcrResultsParser', () => {
   it('should parse artifact main stat type', () => {
     goodOcrResultsMock.forEach((ocrResults, index) => {
       expect(ocrResultsParser.parseToArtifactData(ocrResults).mainStatType).toEqual(importedArtifactDataMock[index].mainStatType);
+    });
+  });
+
+  it('should parse artifact level', () => {
+    goodOcrResultsMock.forEach((ocrResults, index) => {
+      expect(ocrResultsParser.parseToArtifactData(ocrResults).level).toEqual(importedArtifactDataMock[index].level);
+    });
+  });
+
+  fit('should parse artifact substats', () => {
+    goodOcrResultsMock.forEach((ocrResults, index) => {
+      const parsedSubs = ocrResultsParser.parseToArtifactData(ocrResults).subStats;
+      const expectedSubs = importedArtifactDataMock[index].subStats;
+      Object.keys(parsedSubs).forEach((subKey) => {
+        expect(parsedSubs[subKey as SubStats]).toEqual(expectedSubs[subKey as SubStats]);
+      });
     });
   });
 });
--- src/core/domain/ocr-results-parser.ts
+++ src/core/domain/ocr-results-parser.ts
@@ -2,22 +2,54 @@ import { ArtifactType } from './entities/artifact';
 import { StringFormatter } from './mappers/string-formatter';
 import { ArtifactData } from './models/artifact-data';
 import { MainStats } from './models/main-statistics';
+import { SetNames } from './models/sets-with-effects';
+import { SubStats } from './models/sub-statistics';
 
 export class OcrResultsParser {
   private readonly maxStatWords: number = 2;
-  private readonly flatOrPercent: string[] = ['atk', 'def', 'hp'];
+  private readonly flatOrPercentStats: string[] = ['atk', 'def', 'hp'];
 
-  public parseToArtifactData(ocrResults: string[]): Partial<ArtifactData> {
+  public parseToArtifactData(ocrResults: string[]): ArtifactData {
     const type = Object.values(ArtifactType).find((type) => ocrResults[1].toLowerCase().includes(type));
-    const ocrMainStat = this.parseMainStatType(ocrResults, type);
+    const ocrMainStat = this.parseMainStatType(ocrResults[2], type);
     const mainStatType = Object.values(MainStats).find((mainStat) => mainStat === ocrMainStat);
-    return { type, mainStatType };
+    const level = parseInt(ocrResults[4].replace(/\n/g, ''));
+    const ocrSubsStats = [ocrResults[5], ocrResults[6], ocrResults[7], ocrResults[8]].map((ocrSubStat) =>
+      ocrSubStat.toLowerCase().replace(/\n/g, '').replace(/ /g, '').split('+'),
+    );
+    const subsStats = ocrSubsStats.reduce((subsStatsAcc, ocrSubStat) => {
+      const isPercent = ocrSubStat[1] && (ocrSubStat[1].includes('.') || ocrSubStat[1].includes('%'));
+
+      const subStatType = Object.values(SubStats).find((subStat) => {
+        let ocrSubStatType = ocrSubStat[0];
+
+        if (this.flatOrPercentStats.find((stat) => ocrSubStatType.includes(stat))) {
+          ocrSubStatType = `${isPercent ? 'percent' : 'flat'}${ocrSubStatType}`;
+        }
+
+        return ocrSubStatType.replace(/\./g, '').includes(subStat.toLowerCase());
+      });
+      const subStatValue = isPercent ? parseFloat(ocrSubStat[1]) : parseInt(ocrSubStat[1]);
+      if (subStatType) {
+        subsStatsAcc = { ...subsStatsAcc, [`${subStatType}`]: subStatValue };
+      }
+      return subsStatsAcc;
+    }, {});
+
+    return {
+      id: '0',
+      type: type ? type : ArtifactType.flower,
+      set: SetNames.archaicPetra,
+      mainStatType: mainStatType ? mainStatType : MainStats.percentAtk,
+      level,
+      subStats: subsStats,
+    };
   }
 
-  private parseMainStatType(ocrResults: string[], type: ArtifactType | undefined) {
-    const mainWords = ocrResults[2].toLowerCase().replace(/\n/g, '').split(' ');
+  private parseMainStatType(ocrMainStat: string, type: ArtifactType | undefined) {
+    const mainWords = ocrMainStat.toLowerCase().replace(/\n/g, '').split(' ');
 
-    if (this.flatOrPercent.includes(mainWords[0])) {
+    if (this.flatOrPercentStats.includes(mainWords[0])) {
       const artifactHasFlatMain = type === ArtifactType.flower || type === ArtifactType.plume;
       mainWords.unshift(artifactHasFlatMain ? 'flat' : 'percent');
     }
