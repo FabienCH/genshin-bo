0d2bbbc4c560b5efacf039d2b8f0edce8c032301
FabienCH <fabien.charvaz@gmail.com>
2021-04-20 17:51:53 +0200

    feat(weapons): retrieve weapons by type

--- src/core/usescases/weapons-handler.ts
+++ src/core/usescases/weapons-handler.ts
@@ -1,10 +1,14 @@
 import { Levels } from '../domain/models/levels';
+import { WeaponType } from '../domain/models/weapon';
 import { WeaponsRepository } from '../domain/weapons-repository';
 
 export class WeaponsHandler {
   constructor(private readonly weaponsRepository: WeaponsRepository) {}
 
-  public getWeaponsNames(level: Levels = '1'): string[] {
-    return this.weaponsRepository.getAll(level).map((weapon) => weapon.name);
+  public getWeaponsNamesByTypes(type: WeaponType, level: Levels = '1'): string[] {
+    return this.weaponsRepository
+      .getAll(level)
+      .filter((weapon) => weapon.type === type)
+      .map((weapon) => weapon.name);
   }
 }
