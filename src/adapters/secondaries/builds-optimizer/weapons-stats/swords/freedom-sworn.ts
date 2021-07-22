9f048a406f343b5ef9930d50780fceb4d4da5050
FabienCH <fabien.charvaz@gmail.com>
2021-07-22 18:59:47 +0200

    feat(weapons): add 1.6 weapons

---
+++ src/adapters/secondaries/builds-optimizer/weapons-stats/swords/freedom-sworn.ts
@@ -0,0 +1,21 @@
+import { WeaponStats } from '../weapon-stats-type';
+export const freedomSworn: WeaponStats = {
+  name: 'freedomSworn',
+  type: 'sword',
+  levels: {
+    '1': { atk: 46, bonusStat: { elementalMastery: 43 } },
+    '20': { atk: 122, bonusStat: { elementalMastery: 76 } },
+    '20a': { atk: 153, bonusStat: { elementalMastery: 76 } },
+    '40': { atk: 235, bonusStat: { elementalMastery: 111 } },
+    '40a': { atk: 266, bonusStat: { elementalMastery: 111 } },
+    '50': { atk: 308, bonusStat: { elementalMastery: 129 } },
+    '50a': { atk: 340, bonusStat: { elementalMastery: 129 } },
+    '60': { atk: 382, bonusStat: { elementalMastery: 146 } },
+    '60a': { atk: 414, bonusStat: { elementalMastery: 146 } },
+    '70': { atk: 457, bonusStat: { elementalMastery: 164 } },
+    '70a': { atk: 488, bonusStat: { elementalMastery: 164 } },
+    '80': { atk: 532, bonusStat: { elementalMastery: 181 } },
+    '80a': { atk: 563, bonusStat: { elementalMastery: 181 } },
+    '90': { atk: 608, bonusStat: { elementalMastery: 198 } },
+  },
+};
